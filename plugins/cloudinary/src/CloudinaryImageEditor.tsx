06885d96154c2d8e776e9f5bccb3650a6533f847
rockdev55 <steve264225@gmail.com>
2020-01-10 11:51:00 +0100

    Bump version of Compiler CLI in angluar example

--- plugins/cloudinary/src/CloudinaryImageEditor.tsx
+++ plugins/cloudinary/src/CloudinaryImageEditor.tsx
@@ -1,6 +1,6 @@
 /** @jsx jsx */
-import React from 'react'
 import { jsx } from '@emotion/core'
+import React from 'react'
 import { Builder } from '@builder.io/sdk'
 import { Typography, Button } from '@material-ui/core'
 import {
@@ -75,14 +75,10 @@ export default class CloudinaryImageEditor extends React.Component<
   }
 
   private appendMediaLibraryScriptToPlugin() {
-    const previousScript = document.getElementById('cloudinaryScript')
-    if (!previousScript) {
-      const script = document.createElement('script')
-      script.async = true
-      script.src = `https://media-library.cloudinary.com/global/all.js`
-      script.id = 'cloudinaryScript'
-      document.head.appendChild(script)
-    }
+    const script = document.createElement('script')
+    script.async = true
+    script.src = 'https://media-library.cloudinary.com/global/all.js'
+    document.head.appendChild(script)
   }
 
   private areCloudinaryCredentialsNotSet(): boolean {
